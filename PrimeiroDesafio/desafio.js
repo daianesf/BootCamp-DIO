f12d26cd154e7b05cc56861f0ecbf563bc376650
daianesf <dsilvafernandes18@gmail.com>
2024-11-03 02:30:16 -0300

    alterações no desafio - incluindo laço de repetição

--- PrimeiroDesafio/desafio.js
+++ PrimeiroDesafio/desafio.js
@@ -1,24 +1,45 @@
-const nome = "Adrian";
-const experiencia = "2001";
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
 
-let nivel;
+function perguntarHeroi(){
+    rl.question("Digite o nome do herói: ", (nome) =>{
+        rl.question("Digite a quantidade de experiência do Herói: ", (xp) => {
+            xp = Number(xp);
+            let nivel;
+            
+            if(xp < 1000){
+                nivel="Ferro";
+            }else if(xp >= 1001 && xp <= 2000){
+                nivel="Bronze";
+            }else if(xp >= 2001 && xp <= 5000){
+                nivel="Prata";
+            } else if(xp >= 5001 && xp <= 7000){
+                nivel = "ouro";
+            }else if(xp >= 7001 && xp <= 8000){
+                nivel = "Platina";
+            } else if(xp >= 8001 && xp <= 9000){
+                nivel = "Ascendente";
+            } else if(xp >= 9001 && xp <= 10000){
+                nivel = "Imortal";
+            } else{
+                nivel= "Radiante";
+            }
+            
+            console.log(`O herói ${nome} está no nível ${nivel}`)
 
-if (experiencia < 1000) {
-    nivel = "Ferro";
-} else if (experiencia >= 1001 && experiencia <= 2000) {
-    nivel = "Bronze";
-} else if (experiencia >= 2001 && experiencia <= 5000) {
-    nivel = "Prata";
-} else if (experiencia >= 5001 && experiencia <= 7000) {
-    nivel = "Ouro";
-} else if (experiencia >= 7001 && experiencia <= 8000) {
-    nivel = "Platina";
-} else if (experiencia >= 8001 && experiencia <= 9000) {
-    nivel = "Ascendente";
-} else if (experiencia >= 9001 && experiencia <= 10000) {
-    nivel = "Imortal";
-} else {
-    nivel = "Radiante";
+            rl.question("Deseja adicionar outro Herói? (sim/não): ",(resposta) =>{
+                if(resposta.toLowerCase()==="sim"){
+                    perguntarHeroi();
+                }else{
+                    rl.close();
+                }
+            });
+        });
+    });
 }
 
-console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
+perguntarHeroi()
